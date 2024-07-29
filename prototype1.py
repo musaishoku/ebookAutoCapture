8a6c426ea5e85cf47b5de9aa5944fd78f75eedb0
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-29 19:43:23 +0900

    마우스의 좌표를 얻을 수 있게 함.

--- prototype1.py
+++ prototype1.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+import pyautogui
+import time
 
 
 class ebookToPDF:
@@ -13,22 +15,28 @@ class ebookToPDF:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
+        self.positin = 0
 
-        self.pos1 = StringVar()
-        self.pos2 = StringVar()
+        self.posDisplay1 = StringVar()
+        self.posDisplay2 = StringVar()
+        self.posDisplay1.set("[0,0]")
+        self.posDisplay2.set("[0,0]")
 
-        #test
-        self.pos1.set("(0,0)")
-        self.pos2.set("(0,0)")
+        progress = DoubleVar()
+        progress.set(0.0)
 
         ttk.Label(mainframe, text="이미지 좌측 상단 좌표").grid(column=1, row=1, sticky=W)
         ttk.Label(mainframe, text="이미지 우측 하단 좌표").grid(column=1, row=2, sticky=W)
 
-        ttk.Label(mainframe, textvariable=self.pos1).grid(column=2, row=1, sticky=(W, E))
-        ttk.Label(mainframe, textvariable=self.pos2).grid(column=2, row=2, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.posDisplay1).grid(column=2, row=1, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.posDisplay2).grid(column=2, row=2, sticky=(W, E))
 
-        ttk.Button(mainframe, text="좌표위치클릭", command=None).grid(column=3, row=1, sticky=E)
-        ttk.Button(mainframe, text="좌표위치클릭", command=None).grid(column=3, row=2, sticky=E)
+        ttk.Button(mainframe, text="좌표위치클릭", command=self.getPointerPosCallLeft).grid(column=3, row=1, sticky=E)
+        ttk.Button(mainframe, text="좌표위치클릭", command=self.getPointerPosCallRight).grid(column=3, row=2, sticky=E)
 
 
         ttk.Label(mainframe, text="총 페이지 수").grid(column=1, row=3, sticky=W)
@@ -42,8 +50,7 @@ class ebookToPDF:
         ttk.Label(mainframe, textvariable=None).grid(column=2, row=5, sticky=(W, E))
         ttk.Scale(mainframe, orient=HORIZONTAL, length=200, from_=0.1, to=1.0).grid(column=3, row=5, sticky=E)
 
-        progress = DoubleVar()
-        progress.set(10.0)
+        
         ttk.Progressbar(mainframe, orient=HORIZONTAL, length=500, mode='determinate', maximum = 100.0, variable=progress).grid(column=2, row=6, sticky=(W,E))
 
         
@@ -53,9 +60,32 @@ class ebookToPDF:
         for child in mainframe.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
         # feet_entry.focus()
-        root.bind("<Return>", None)
-        
+
+        root.bind("<Key-space>", lambda : print("Hello World"))
     
+
+    def getPointerPosCallLeft(self,*args): #Tkinter를 통해서 호출되는 메서드는 매개변수로 반드시 *args를 가지고 있어야 함.
+        print("getPointerPosCallLeft")
+        self.position = 1
+        root.bind("<Key-space>", self.getPointerPos)
+
+    def getPointerPosCallRight(self,*args):
+        print("getPointerPosCallRight")
+        self.position = 2
+        root.bind("<Key-space>", self.getPointerPos)
+
+    def getPointerPos(self,*args):
+        posx,posy = pyautogui.position()
+        if(self.position == 1):
+            self.x1 = posx
+            self.y1 = posy
+            self.posDisplay1.set(str([posx,posy]))
+        if(self.position == 2):
+            self.x2 = posx
+            self.y2 = posy
+            self.posDisplay2.set(str([posx,posy]))
+        root.bind("<Key-space>", None)
+
 
 root = Tk()
 ebookToPDF(root)#FeetToMeters인스턴스를 생성하는 과정에서 생성자 함수가 호출되고, root에 모든 설정을 끝냄.
