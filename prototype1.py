3a2860a00abb6ccfb82fab98230f0b0f30e53e7d
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-30 20:29:28 +0900

    거의 완성형에 가까워짐. 다음 페이지로 넘어가는 옵션도 추가

--- prototype1.py
+++ prototype1.py
@@ -36,20 +36,22 @@ class ebookToPDF:
 
         self.captureSpeed = DoubleVar()
 
+        self.moveToNextPageOption = IntVar()
+
         self.progress = DoubleVar()
         self.progress.set(0.0)
 
         root.title("ebookToPDF")
-        root.geometry("390x310")
+        root.geometry("")#이렇게 해야 내부 위젯들 사이즈에 맞게 창의 크기가 자동으로 조절됨. https://stackoverflow.com/questions/50955987/auto-resize-tkinter-window-to-fit-all-widgets
         #root.resizable(width=False, height=False)      
 
-        contents = ttk.Frame(root, borderwidth=5, padding="3 3 12 12")
+        contents = ttk.Frame(root, padding="3 3 3 3")
         contents.grid(column=0, row=0, sticky=(N, W, E, S))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        ttk.Label(contents, text="이미지 좌측 상단 좌표", ).grid(column=1, row=1, sticky=W)
-        ttk.Label(contents, text="이미지 우측 하단 좌표", ).grid(column=1, row=2, sticky=W)
+        ttk.Label(contents, text="좌측 상단 좌표", ).grid(column=1, row=1, sticky=W)
+        ttk.Label(contents, text="우측 하단 좌표", ).grid(column=1, row=2, sticky=W)
         ttk.Label(contents, textvariable=self.posDisplay1, width=10).grid(column=2, row=1, sticky=(W, E))
         ttk.Label(contents, textvariable=self.posDisplay2, width=10).grid(column=2, row=2, sticky=(W, E))
         ttk.Button(contents, text="좌표 설정", command=self.getPointerPosCallLeft).grid(column=3, row=1, sticky=(W, E))
@@ -57,18 +59,22 @@ class ebookToPDF:
 
         ttk.Label(contents, text="총 페이지 수").grid(column=1, row=3, sticky=W)
         ttk.Label(contents, text="파일 이름").grid(column=1, row=4, sticky=W)
-        ttk.Entry(contents, width=20, textvariable=self.pages).grid(column=3, row=3, sticky=(W, E))
-        ttk.Entry(contents, width=20, textvariable=self.name).grid(column=3, row=4, sticky=(W, E))
+        ttk.Entry(contents, width=10, textvariable=self.pages).grid(column=3, row=3, sticky=(W, E))
+        ttk.Entry(contents, width=10, textvariable=self.name).grid(column=3, row=4, sticky=(W, E))
 
         ttk.Label(contents, text="캡쳐 간격(초)").grid(column=1, row=5, sticky=W)
         ttk.Label(contents, textvariable=self.captureSpeed,width=3).grid(column=2, row=5, sticky=(W, E))
         ttk.Scale(contents, orient=HORIZONTAL, length=100, from_=0.1, to=1.0, variable=self.captureSpeed).grid(column=3, row=5, sticky=(W,E))
-        
-        ttk.Progressbar(contents, orient=HORIZONTAL,length=30, mode='determinate', maximum = 10000, variable=self.progress).grid(column=1, row=6, columnspan=3, sticky=(W,E))
 
-        ttk.Button(contents, text="작업 시작", command=self.captureCall).grid(column=1, row=7,columnspan=3, sticky=(W,E))
-        ttk.Button(contents, text="저장 경로 설정", command=self.getDirPath).grid(column=1, row=8,columnspan=3, sticky=(W,E))
-        ttk.Label(contents, text="경로", textvariable=self.dirPath, width=51).grid(column=1, row=9,columnspan=3, sticky=W)
+        ttk.Label(contents, text="다음 페이지 이동").grid(column=1, row=6, sticky=W)
+        ttk.Radiobutton(contents, text="마우스 클릭", variable=self.moveToNextPageOption, value=0).grid(column=2, row=6, sticky=W)
+        ttk.Radiobutton(contents, text="키보드 방향키", variable=self.moveToNextPageOption, value=1).grid(column=3, row=6, sticky=W)
+
+        ttk.Progressbar(contents, orient=HORIZONTAL,length=30, mode='determinate', maximum = 10000, variable=self.progress).grid(column=1, row=7, columnspan=3, sticky=(W,E))
+
+        ttk.Button(contents, text="작업 시작", command=self.captureCall).grid(column=1, row=8,columnspan=3, sticky=(W,E))
+        ttk.Button(contents, text="저장 경로 설정", command=self.getDirPath).grid(column=1, row=9,columnspan=3, sticky=(W,E))
+        ttk.Label(contents, text="경로", textvariable=self.dirPath, width=51).grid(column=1, row=10,columnspan=3, sticky=W)
 
         for child in contents.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
@@ -101,12 +107,9 @@ class ebookToPDF:
         print(self.dirPath.get())
 
     def captureCall(self, *args):
-        #time.sleep(2)#2초후 실행
-    
         capture = Capture(self,root)#이 인스턴스를 통째로 넘겨줌.
 
         root.after(2000, capture.process)
-        #capture.process()
 
 
 
@@ -119,9 +122,11 @@ class Capture:
         self.dirpath = ebookToPDF.dirPath.get().replace("/","\\")
         self.captureSpeed = int(round(ebookToPDF.captureSpeed.get(),1)*1000)
         self.region = (ebookToPDF.x1,ebookToPDF.y1,ebookToPDF.x2-ebookToPDF.x1,ebookToPDF.y2-ebookToPDF.y1)
+
         self.progress = ebookToPDF.progress
         self.progress.set(0.0)
-        print(self.progress.get())
+
+        self.moveToNextPage = self.selectMoveToNextPageOption(ebookToPDF.moveToNextPageOption.get())
 
         self.count = 1
 
@@ -130,13 +135,12 @@ class Capture:
         if (self.count<=self.pages):
             self.capture()
             self.progress.set(self.progress.get()+(10000/self.pages))
-            print(self.progress.get())
-            self.moveToNextPageWithKey()
+
+            self.moveToNextPage()
             
             root.after(self.captureSpeed,self.process)
             #tkinter는 time.sleep을 쓰는게 좋지 않다고 함. 이렇게 해야 progressbar가 지속적으로 업데이트 됨.
             #https://stackoverflow.com/questions/51298758/tkinter-updating-progressbar-when-a-function-is-called
-
 
     def capture(self):
         now = datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -150,14 +154,23 @@ class Capture:
             screenshot = sct.grab(monitor)
             mss.tools.to_png(screenshot.rgb, screenshot.size, output=save_dir)
             
+
+    def selectMoveToNextPageOption(self,num):
+        if num == 0:
+            return self.moveToNextPageWithKey
+        elif num   == 1:
+            return self.moveToNextPageWithClick
+        
     #다음페이지로 넘겨주는 함수들
     def moveToNextPageWithKey(self):
         pdi.keyDown("right")
         time.sleep(0.1)
         pdi.keyUp("right")
+        print("딸칵")
 
     def moveToNextPageWithClick(self):
         pdi.leftClick()
+        print("클릭")
 
 
 root = Tk()
