37c4e7495bb973e4e92e1d1116c3a105c537a944
gamja3 <dwkimdwkim1005@gmail.com>
2024-07-30 20:29:28 +0900

    Capture개선, 프로그레스바 실시간 업데이트 성공

--- prototype1.py
+++ prototype1.py
@@ -8,6 +8,7 @@ import mss.tools
 import os
 import time
 import pydirectinput as pdi
+from datetime import datetime
 
 #잘가....
 
@@ -63,7 +64,7 @@ class ebookToPDF:
         ttk.Label(contents, textvariable=self.captureSpeed,width=3).grid(column=2, row=5, sticky=(W, E))
         ttk.Scale(contents, orient=HORIZONTAL, length=100, from_=0.1, to=1.0, variable=self.captureSpeed).grid(column=3, row=5, sticky=(W,E))
         
-        ttk.Progressbar(contents, orient=HORIZONTAL,length=30, mode='determinate', maximum = float(self.pages.get()), variable=self.progress).grid(column=1, row=6, columnspan=3, sticky=(W,E))
+        ttk.Progressbar(contents, orient=HORIZONTAL,length=30, mode='determinate', maximum = 10000, variable=self.progress).grid(column=1, row=6, columnspan=3, sticky=(W,E))
 
         ttk.Button(contents, text="작업 시작", command=self.captureCall).grid(column=1, row=7,columnspan=3, sticky=(W,E))
         ttk.Button(contents, text="저장 경로 설정", command=self.getDirPath).grid(column=1, row=8,columnspan=3, sticky=(W,E))
@@ -94,29 +95,61 @@ class ebookToPDF:
             self.y2 = posy
             self.posDisplay2.set(str([posx,posy]))
         #root.unbind("<Key-space>") 스페이스바를 여러번 눌러서 좌표를 수정할 수 있게함.
-        
-    def calculateRegion(self):
-        #topLeftX, topRightY, width, height
-        self.region = (self.x1,self.y1,self.x2-self.x1,self.y2-self.y1)
 
     def getDirPath(self, *args):
         self.dirPath.set(filedialog.askdirectory())
         print(self.dirPath.get())
 
-    def captureCall(self, *args, moveToNextPageMethod):
-        time.sleep(2)#2초후 실행
+    def captureCall(self, *args):
+        #time.sleep(2)#2초후 실행
+    
+        capture = Capture(self,root)#이 인스턴스를 통째로 넘겨줌.
 
-        self.progress.set(0)#진행률 표시바 초기화
-        self.calculateRegion()#캡처 영역 계산
-        capture = Capture(self)#이 인스턴스를 통째로 넘겨줌.
+        root.after(2000, capture.process)
+        #capture.process()
 
-        for i in range(self.pages.get()):
-            capture.capture()
-            self.progress.set(self.progress.get()+1)
 
-            time.sleep(self.captureSpeed.get())
-            moveToNextPageMethod()
 
+class Capture:
+    def __init__(self,ebookToPDF,root):
+        self.root = root
+        self.region = ebookToPDF.region
+        self.pages = ebookToPDF.pages.get()
+        self.name = ebookToPDF.name.get()
+        self.dirpath = ebookToPDF.dirPath.get().replace("/","\\")
+        self.captureSpeed = int(round(ebookToPDF.captureSpeed.get(),1)*1000)
+        self.region = (ebookToPDF.x1,ebookToPDF.y1,ebookToPDF.x2-ebookToPDF.x1,ebookToPDF.y2-ebookToPDF.y1)
+        self.progress = ebookToPDF.progress
+        self.progress.set(0.0)
+        print(self.progress.get())
+
+        self.count = 1
+
+    def process(self):
+
+        if (self.count<=self.pages):
+            self.capture()
+            self.progress.set(self.progress.get()+(10000/self.pages))
+            print(self.progress.get())
+            self.moveToNextPageWithKey()
+            
+            root.after(self.captureSpeed,self.process)
+            #tkinter는 time.sleep을 쓰는게 좋지 않다고 함. 이렇게 해야 progressbar가 지속적으로 업데이트 됨.
+            #https://stackoverflow.com/questions/51298758/tkinter-updating-progressbar-when-a-function-is-called
+
+
+    def capture(self):
+        now = datetime.now().strftime("%Y%m%d-%H%M%S")
+        save_dir = str(f"{self.dirpath}\\{self.name}[{self.count}]{now}.png")
+        print(save_dir)
+
+        self.count += 1
+
+        with mss.mss() as sct:
+            monitor = {"top": self.region[1], "left": self.region[0], "width": self.region[2], "height": self.region[3]}
+            screenshot = sct.grab(monitor)
+            mss.tools.to_png(screenshot.rgb, screenshot.size, output=save_dir)
+            
     #다음페이지로 넘겨주는 함수들
     def moveToNextPageWithKey(self):
         pdi.keyDown("right")
@@ -125,29 +158,6 @@ class ebookToPDF:
 
     def moveToNextPageWithClick(self):
         pdi.leftClick()
-
-
-
-class Capture:
-    def __init__(self,ebookToPDF):
-        self.region = ebookToPDF.region
-        self.pages = ebookToPDF.pages.get()
-        self.name = ebookToPDF.name.get()
-        self.dirpath = ebookToPDF.dirPath.get().replace("/","\\")
-        self.captureSpeed = round(ebookToPDF.captureSpeed.get(),1)
-        self.count = 0
-
-    def capture(self):
-
-        save_dir = self.dirpath+"\\"+self.name+str(self.count)+".png"
-        self.count += 1
-
-        with mss.mss() as sct:
-            monitor = {"top": self.region[1], "left": self.region[0], "width": self.region[2], "height": self.region[3]}
-            screenshot = sct.grab(monitor)
-            mss.tools.to_png(screenshot.rgb, screenshot.size, output=save_dir)
-            
-
 
 
 root = Tk()
